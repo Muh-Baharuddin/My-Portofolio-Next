6911cf13f93b4e61ae2eefeaf6ea2ac4d0783fb5
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-12-09 19:10:53 +0700

    update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,30 @@
+"use client"
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+  const [isHamburgerActive, setHamburgerActive] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerActive(!isHamburgerActive);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      const hamburger = document.getElementById('hamburger');
+      const navMenu = document.getElementById('nav-menu');
+
+      if (hamburger && navMenu && !hamburger.contains(e.target as Node) && !navMenu.contains(e.target as Node)) {
+        setHamburgerActive(false);
+      }
+    };
+
+    window.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
     <header className="absolute top-0 left-0 z-10 flex w-full items-center bg-transparent">
       <div className="container">
@@ -7,7 +33,7 @@ const NavBar = () => {
             <a href="#home" className="block py-6 text-lg font-bold">Muh Baharuddin Portofolio</a>
           </div>
           <div className="flex items-center px-4">
-            <button id="hamburger" name="hamburger" type="button" className="absolute right-4 block lg:hidden">
+            <button id="hamburger" name="hamburger" type="button" className="absolute right-4 block lg:hidden" onClick={toggleHamburger}>
               <span className="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
               <span className="hamburger-line transition duration-300 ease-in-out"></span>
               <span className="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
@@ -15,7 +41,7 @@ const NavBar = () => {
 
             <nav
               id="nav-menu"
-              className="absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark dark:shadow-slate-500 lg:static lg:block lg:max-w-full lg:rounded-none lg:bg-transparent lg:shadow-none lg:dark:bg-transparent"
+              className={`absolute right-4 top-full ${isHamburgerActive ? '' : 'hidden'} w-full max-w-[250px] rounded-lg bg-slate-800 py-5 shadow-lg dark:bg-dark dark:shadow-slate-500 lg:static lg:block lg:max-w-full lg:rounded-none lg:bg-transparent lg:shadow-none lg:dark:bg-transparent`}
             >
               <ul className="block lg:flex">
                 <li className="group">
---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R15AeBO9WM/Ie8HsfREhMKhuvV2reSZatKpw3N41J8M=\"\n}"
